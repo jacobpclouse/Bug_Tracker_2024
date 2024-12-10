f421bd73d5889e6109e1bfb2b78f4c7f0f2cad5f
Jacob Clouse <clouse.jp@gmail.com>
2024-12-09 19:48:13 -0500

    added original issues getters and setters so i can potentially use this with the stackoverflowapi

--- BugTrackerDN/Models/Issue.cs
+++ BugTrackerDN/Models/Issue.cs
@@ -8,3 +8,22 @@ public class Issue
     public string Title { get; set; }
     public string Reporter { get; set; }
 }
+
+// add more attributes like the following: 
+/*
+    public class Issue
+    {
+        public int Id { get; set; }
+        public string IssueReporter { get; set; }
+        public DateTime IssueDate { get; set; }
+        public string IssueTitle { get; set; }
+        public string IssueDescription { get; set; }
+        public string IssueType { get; set; }
+        public string ReporterPhone { get; set; }
+        public string ReporterEmail { get; set; }
+        public string IssueLocation { get; set; }
+        public bool IssueAssigned { get; set; }
+        public bool IssueResolved { get; set; }
+        public string IssueAssignee { get; set; }
+    }
+*/
